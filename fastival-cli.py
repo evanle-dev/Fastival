87f5737f273a9d11f44724b2f6c9cf7668dc6133
Rowan <rowan@enclave.vn>
2020-09-17 09:12:38 +0700

    Update fastival cli

--- fastival-cli.py
+++ fastival-cli.py
@@ -1,23 +1,29 @@
 import subprocess
+import sys
 
 import inquirer
 
+RUN_SERVER = 'run server'
+DO_SEEDER = 'do seeder'
+DO_MIGRATION = 'do migration'
+
 choices = [
-    'run',
-    'migrate',
-    'seed'
+    RUN_SERVER,
+    DO_SEEDER,
+    DO_MIGRATION
 ]
 
 questions = [
-    inquirer.List('task',
-                  message="What do you want to do?",
-                  choices=choices,
-                  ),
+    inquirer.List(
+        name='task',
+        message="What do you want to do?",
+        choices=choices,
+    ),
 ]
 answers = inquirer.prompt(questions)
 
-if answers['task'] == 'run':
+if answers['task'] == RUN_SERVER:
     try:
         subprocess.call(['sh', './scripts/runserver.sh'])
     except KeyboardInterrupt:
-        print('Interrupted')
+        sys.exit(0)
