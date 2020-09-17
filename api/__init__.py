c457133f2cbad5e73fffc50701e4f69a2a9897eb
Rowan <rowan@enclave.vn>
2020-09-17 09:12:59 +0700

    Add feature versioning

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import importlib
+
 from fastapi import FastAPI
+from fastapi_versioning import VersionedFastAPI
+
+__version__ = '0.1.0'
 
 app = FastAPI()
 
+version = __version__.split('.')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+importlib.import_module('api.routes.v{0}_{1}'.format(version[1], version[2]))
+
+app = VersionedFastAPI(app)
