59214b53305ca84ad79ff8bd905e52f957d97071
Rowan <rowan@enclave.vn>
2020-09-17 09:12:06 +0700

    Chores

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "$ENV" != "" ]; then
-  echo "You are running on $ENV environment"
-  uvicorn api:app --host 0.0.0.0 --port 80
-else
+if [ "$ENV" = "" ]; then
   echo "You are running on local environment"
   uvicorn api:app --reload
+else
+  echo "You are running on $ENV environment"
+  uvicorn api:app --host 0.0.0.0 --port 80
 fi
